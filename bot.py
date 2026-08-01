8a07e15207859726ab4e5e93ffd5118a52ea74ea
NITESH <apnanitesh0@gmail.com>
2026-08-02 02:49:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import requests
 
 # =========================== CONFIGURATION ===========================
 BOT_TOKEN = "8967548495:AAGt5iQEjHhidU8uBroFGJ_dT9J7r6L2Bws"   # <-- YOUR BOT TOKEN
-EXTERNAL_API_URL = "c863923467e2de1e4e25f1ff7e9cfcc9"  # <-- NumVerify URL: http://apilayer.net/api/validate?access_key=KEY&num=
+EXTERNAL_API_URL = "http://apilayer.net/api/validate?access_key=c863923467e2de1e4e25f1ff7e9cfcc9&num="  # <-- NumVerify URL: http://apilayer.net/api/validate?access_key=KEY&num=
 
 # =========================== TELEGRAM API ===========================
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
